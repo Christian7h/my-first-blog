62d5b96930e5572788357f7f7ef4bccb64672f04
cristian <cristianvillalobos666@gmail.com>
2024-01-12 23:27:32 -0300

    todo de nuevo

--- pruebapag/settings.py
+++ pruebapag/settings.py
@@ -118,8 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'static'
 
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
