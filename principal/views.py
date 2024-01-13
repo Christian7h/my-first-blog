3cf5ae3b13b99158985c0c4b2d9cd9004d046f3e
cristian <cristianvillalobos666@gmail.com>
2024-01-12 21:37:57 -0300

    Blog

--- principal/views.py
+++ principal/views.py
@@ -1,24 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404,redirect
 from django.utils import timezone
-from .models import Post
-from django.shortcuts import render
-from .models import Post
-from django.shortcuts import render,redirect
-from django.utils import timezone
-from .models import Post
 from .forms import PostForm
-
-from django.shortcuts import render
-from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404
-def post_list(request):
+
+# Create your views here.
+
+def inicio(request):
+    # Lógica para recuperar y mostrar contenido relacionado con la salud
+    return render(request, 'inicio.html')
+
+def blog(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog.html', {'posts': posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'post_detail.html', {'post': post})
 
 def post_new(request):
     if request.method == "POST":
@@ -31,7 +29,7 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'post_edit.html', {'form': form})
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -45,4 +43,4 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'post_edit.html', {'form': form})
--- pruebapag/urls.py
+++ pruebapag/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 # pruebapag/urls.py
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from principal import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('principal.urls')),
+    path('', views.inicio, name='inicio'),
+    path('blog/',views.blog,name='blog'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
